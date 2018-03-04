3a39c5390d71648b754980031a08c904ccbea2d7
Chaoi Chen <chaoichen@icloud.com>
2018-03-03 23:15:46 -0800

    rewrite it as a function

--- Prerequistes/SquareTheValues/SquareTheValues.js
+++ Prerequistes/SquareTheValues/SquareTheValues.js
@@ -1,6 +1,9 @@
 let X = [1, 5, 10, -2];
 
-for(let i = 0; i < X.length; i++)
-	X[i] = X[i] * X[i];
+function SquareTheValues(arr){
+	for(let i = 0; i < X.length; i++)
+		X[i] = X[i] * X[i];
+	return arr;
+}
 
-console.log(X);
+console.log(SquareTheValues(X));
