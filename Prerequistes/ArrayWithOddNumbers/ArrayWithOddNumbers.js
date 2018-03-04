0f40f8610a1307df7a1f42ee9d2ba59fb3996896
Chaoi Chen <chaoichen@icloud.com>
2018-03-03 23:18:48 -0800

    rewirte into a functino

--- Prerequistes/ArrayWithOddNumbers/ArrayWithOddNumbers.js
+++ Prerequistes/ArrayWithOddNumbers/ArrayWithOddNumbers.js
@@ -1,6 +1,9 @@
-var Y = [];
+function ArrayWithOddNumbers(){
+	var Y = [];
 
-for(let i=1; i<=255; i++)
-	if(i % 2 == 1)	Y.push(i);
+	for(let i=1; i<=255; i++)
+		if(i % 2 == 1)	Y.push(i);
+	return Y;
+}
 
-console.log(Y);
+console.log(ArrayWithOddNumbers());
