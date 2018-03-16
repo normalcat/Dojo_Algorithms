21b611708c62e98ffa38bd5695c9a41f479ff259
Chaoi Chen <chaoichen@icloud.com>
2018-03-15 21:34:31 -0700

    try to work on insert

--- Data_Structures/Trie/Trie.js
+++ Data_Structures/Trie/Trie.js
@@ -7,18 +7,32 @@ function Trie() {
   this.root = new Node("A", null);
 }
 Trie.prototype.insert = function(word, value) {
+	var i,j;
 	pt = this.root;
 	if(pt.chr!=word[0])	return null;	//it should all be starting "A"
 
-	for(let i=1; i<word.length-1;i++){
-		if(pt.next.chr == word[i]){
-			for(let j=0; j<pt.next.length;j++)
-				if(pt.next[j].char == word[i+1])
-					pt = pt.next[j];
-		}else pt.next.push(Node(word[i],null))
+	for(i=1; i<word.length-1;i++){
+		if(pt.next.length > 0){
+			console.log(pt.next.length);
+			console.log(pt.next[0].char);
+//			for(j=0; j<pt.next.length;j++)
+//				if(pt.next[j].char == word[i+1])
+//					pt = pt.next[j];
+		}else pt.next.push(Node(word[i],null));
 	}
-	pt.next.push(Node(word(word[i],value)));
-  // your code here
+	pt.next.push(Node(word[i],value));
+}
+Trie.prototype.show = function(X){
+	if(!X)	return null;
+	
+	console.log(X.chr);
+	if(X.value){
+		console.log("---"+X.value);
+		return ;
+	}
+
+	for(let i=0; i<X.next.length;i++)
+		this.show(X.next[i]);
 }
 Trie.prototype.get = function(word) {
   // your code here (outputs value of word)
