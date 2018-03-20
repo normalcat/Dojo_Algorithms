5e602f5902ade82951d29fbc6d3d33b94404524c
Chaoi Chen <chaoichen@icloud.com>
2018-03-19 21:38:12 -0700

    add comments

--- Data_Structures/BinarySearchTreeIII/queue.js
+++ Data_Structures/BinarySearchTreeIII/queue.js
@@ -1,23 +1,23 @@
 var Queue = (function(){
-  function Queue() {
-    this.dataStore = [];
-    this.back = 0;
+  function Queue() {        //constructor
+    this.dataStore = [];    //empty array
+    this.back = 0;          //default index
   }
   Queue.prototype.enqueue = function(item) {
-    this.dataStore[this.back] = item;
-    this.back++;
+    this.dataStore[this.back] = item;   //assign the item to the current index
+    this.back++;                        //increment by one
   }
   Queue.prototype.dequeue = function() {
-    var output = this.dataStore[0];
-    for(var i = 1; i < this.back; i++) {
+    var output = this.dataStore[0];     //always get the first one
+    for(var i = 1; i < this.back; i++) {//move everything one element forward
       this.dataStore[i - 1] = this.dataStore[i];
     }
-    this.back--;
-    return output;
+    this.back--;                        //decrease the current index
+    return output;                      //return the value
   }
   Queue.prototype.display = function() {
     for(var i = 0; i < this.back; i++) {
-      console.log(this.dataStore[i]);
+      console.log(this.dataStore[i]);   //display the entire queue
     }
   }
   return Queue;
